3c46b4a98b242db15f8444fd9d77c319564a0afc
nayemuzzaman <nayemzaman@gmail.com>
2023-06-10 14:37:51 +0900

    build screen

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,85 @@
+import 'package:chat_gpt/constants/constants.dart';
+import 'package:chat_gpt/services/assets_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final bool _isTyping = true;
+  late TextEditingController textEditingController;
+
+  @override
+  void initState() {
+    textEditingController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.asset(AssetsManager.openailogo),
+        ),
+        title: const Text("ChatGPT"), actions: [
+          IconButton(
+            onPressed: () {}, 
+            icon: const Icon(Icons.send, color: Colors.white,))
+        ],
+      ),
+      body: SafeArea(
+          child: Column(
+        children: [
+          Flexible(
+            child: ListView.builder(
+                itemCount: 6,
+                itemBuilder: (context, index) {
+                  return const Text("Hello this is a text");
+                }),
+          ),
+          if (_isTyping) ...[
+            const SpinKitThreeBounce(
+              color: Colors.white,
+              size: 18,
+            ),
+            SizedBox(height: 15,),
+            Material(
+              color: cardColor,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        style: const TextStyle(color: Colors.white),
+                       controller: textEditingController,
+                       onSubmitted: (value) {
+                        // send message
+                       },
+                       decoration: const InputDecoration.collapsed(
+                         hintText: "How can i helo you",
+                         hintStyle: TextStyle(color: Colors.grey)),
+                      ),
+                    ),
+                    IconButton(onPressed: () {
+                      
+                    }, icon: const Icon(Icons.send, color: Colors.white,))
+                  ],
+                ),
+              ),
+            )
+          ]
+        ],
+      )),
+    );
+  }
+}
